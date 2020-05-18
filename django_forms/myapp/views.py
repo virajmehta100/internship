e3a59c18a6f993253db96ae1d669a198dbb7604f
Viraj Mehta <viraj1messi10@gmail.com>
2020-05-18 12:38:02 +0530

    second draft

---
+++ django_forms/myapp/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ContactForm,SnippetForm
+
+
+# Create your views here.
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['name']
+            email=form.cleaned_data['email']
+
+            print(name)
+    else:
+        form=ContactForm()
+    return render(request,'forms.html',{'form': form})
+
+def snippet_detail(request):
+
+    if request.method == 'POST':
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form=SnippetForm()
+    return render(request,'forms.html',{'form': form})
